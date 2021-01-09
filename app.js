cf5ef4fb7cc9f0b4572945647205385a63376442
Ronald Buzaglo Pessoa <senseironald@gmail.com>
2021-01-09 13:54:35 -0400

    initial commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@
 //CONFIGURAÇÕES 
     //SESSÃO  
     app.use(session({
-        secret: "cursodenode",
+        secret: "senseironald@gmail.com",
         resave: true,
         saveUninitialized:true
     }))
